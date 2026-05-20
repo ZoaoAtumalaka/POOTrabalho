4372bb076b23b52f411a8d25408f4a9fef1086b4
AromCoronil <coronil.arom1@gmail.com>
2026-05-20 15:21:57 -0300

    Implementei a classe Acoes

--- projeto/src/Acoes.java
+++ projeto/src/Acoes.java
@@ -1,3 +1,9 @@
-public class Acoes {
-    
+public interface Acoes{
+    abstract public boolean verificarVida ();
+    abstract public void matar();
+    abstract public void reanimar();
+    abstract public int[] executarMissao();
+    abstract public boolean darXp(int xp);
+    abstract public void evoluir(String atributo);
+    abstract public void descansar();
 }
