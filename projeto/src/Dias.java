fad8cd9596db2ba17ab66316f30d01efe4e2e1c8
AromCoronil <coronil.arom1@gmail.com>
2026-05-22 16:25:13 -0300

    Implementando Dias parte 1

--- projeto/src/Dias.java
+++ projeto/src/Dias.java
@@ -1,3 +1,18 @@
+import java.util.ArrayList;
+
 public class Dias {
-    
+        ArrayList<Cenarios> cenariosFaceis = new ArrayList<>();
+        ArrayList<Cenarios> cenariosMedios = new ArrayList<>();
+        ArrayList<Cenarios> cenariosDificeis = new ArrayList<>();
+        ArrayList<Cenarios> quantidadeMissoesExecutadas = new ArrayList<>();
+        ArrayList<Cenarios> quantidadeMissoesDia = new ArrayList<>();
+        ArrayList<Cenarios> culpadoFalha = new ArrayList<>();
+
+
+        public void passarDia(){
+        
+        }
+        
+
+
 }
