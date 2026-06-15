9267e5c6027fdca62778302ca066e1726bb9c8aa
AromCoronil <coronil.arom1@gmail.com>
2026-06-15 13:28:11 -0300

    Tentando arrumar a dificuldade

--- java/Dias.java
+++ java/Dias.java
@@ -37,32 +37,32 @@ public class Dias implements Serializable {
         this.diaAtual = 0;
 
 // CENARIOS FACEIS
-        this.cenariosFaceis.add(new Cenarios("Resgatar gatinho cibernético na árvore", 1, 2, 1, 1, 2, 3, 2, 1, 100));
-        this.cenariosFaceis.add(new Cenarios("Ajudar Velho Pucrson: O sumiço da dentadura", 1, 1, 3, 1, 3, 4, 3, 1, 100));
-        this.cenariosFaceis.add(new Cenarios("Recuperar Wi-Fi do bar local", 1, 1, 2, 1, 1, 2, 1, 1, 100));
-        this.cenariosFaceis.add(new Cenarios("Espantar pombos mutantes da praça", 2, 2, 1, 2, 2, 3, 2, 1, 100));
-        this.cenariosFaceis.add(new Cenarios("Entregar pizza fria na casa branca", 1, 4, 1, 1, 4, 1, 4, 1, 100));
-        this.cenariosFaceis.add(new Cenarios("Desativar despertador barulhento da senhora de idade", 1, 2, 2, 1, 1, 2, 1, 1, 100));
-        this.cenariosFaceis.add(new Cenarios("Encontrar os óculos do prefeito", 1, 1, 3, 1, 2, 4, 2, 1, 100));
-        this.cenariosFaceis.add(new Cenarios("Limpar gosma verde dos esgotos", 3, 1, 1, 3, 3, 5, 3, 1, 100));
-        this.cenariosFaceis.add(new Cenarios("Capturar animais do zoológico que sumiram", 2, 3, 2, 2, 4, 4, 4, 1, 100));
-        this.cenariosFaceis.add(new Cenarios("Escoltar carrinho de feira da vovó", 2, 1, 1, 3, 3, 4, 3, 1, 100));
+        this.cenariosFaceis.add(new Cenarios("Resgatar gatinho cibernético na árvore",              2, 3, 2, 2, 2, 3, 2, 1, 100));
+        this.cenariosFaceis.add(new Cenarios("Ajudar Velho Pucrson: O sumiço da dentadura",          2, 2, 4, 2, 3, 4, 3, 1, 100));
+        this.cenariosFaceis.add(new Cenarios("Recuperar Wi-Fi do bar local",                         2, 2, 3, 2, 1, 2, 1, 1, 100));
+        this.cenariosFaceis.add(new Cenarios("Espantar pombos mutantes da praça",                    3, 3, 2, 3, 2, 3, 2, 1, 100));
+        this.cenariosFaceis.add(new Cenarios("Entregar pizza fria na casa branca",                   2, 5, 2, 2, 4, 1, 4, 1, 100));
+        this.cenariosFaceis.add(new Cenarios("Desativar despertador barulhento da senhora de idade", 2, 3, 3, 2, 1, 2, 1, 1, 100));
+        this.cenariosFaceis.add(new Cenarios("Encontrar os óculos do prefeito",                      2, 2, 5, 2, 2, 4, 2, 1, 100));
+        this.cenariosFaceis.add(new Cenarios("Limpar gosma verde dos esgotos",                       5, 2, 2, 5, 3, 5, 3, 1, 100));
+        this.cenariosFaceis.add(new Cenarios("Capturar animais do zoológico que sumiram",            3, 4, 3, 3, 4, 4, 4, 1, 100));
+        this.cenariosFaceis.add(new Cenarios("Escoltar carrinho de feira da vovó",                   3, 2, 2, 4, 3, 4, 3, 1, 100));
 
 // CENARIOS MÉDIOS
-        this.cenariosMedios.add(new Cenarios("Conter surto de IA em uma empresa", 2, 3, 5, 2, 5, 8, 5, 2, 200));
-        this.cenariosMedios.add(new Cenarios("Ajudar Velho Pucrson: A conspiração do asilo", 3, 2, 4, 3, 4, 6, 4, 1, 200));
-        this.cenariosMedios.add(new Cenarios("Para briga em bar de vilões", 5, 3, 1, 4, 2, 5, 2, 2, 200));
-        this.cenariosMedios.add(new Cenarios("Escoltar cantor famoso dentro do homelander music show", 4, 4, 3, 4, 6, 10, 6, 2, 200));
-        this.cenariosMedios.add(new Cenarios("Rastrear contrabandistas de composto V falso", 3, 4, 5, 3, 5, 7, 5, 1, 200));
-        this.cenariosMedios.add(new Cenarios("Interromper ritual de ceita", 4, 3, 4, 4, 4, 6, 4, 2, 200));
-        this.cenariosMedios.add(new Cenarios("Impedir the boys dentro do hospício", 5, 5, 3, 5, 5, 9, 5, 2, 200));
+        this.cenariosMedios.add(new Cenarios("Conter surto de IA em uma empresa",                          5, 6, 9, 5,  5, 12,  5, 2, 200));
+        this.cenariosMedios.add(new Cenarios("Ajudar Velho Pucrson: A conspiração do asilo",               6, 5, 8, 6,  4,  9,  4, 2, 200));
+        this.cenariosMedios.add(new Cenarios("Para briga em bar de vilões",                                9, 6, 3, 8,  2,  8,  2, 2, 200));
+        this.cenariosMedios.add(new Cenarios("Escoltar cantor famoso dentro do homelander music show",     7, 7, 6, 7,  6, 14,  6, 2, 200));
+        this.cenariosMedios.add(new Cenarios("Rastrear contrabandistas de composto V falso",               6, 8, 9, 6,  5, 11,  5, 2, 200));
+        this.cenariosMedios.add(new Cenarios("Interromper ritual de ceita",                                8, 6, 8, 8,  4, 10,  4, 2, 200));
+        this.cenariosMedios.add(new Cenarios("Impedir the boys dentro do hospício",                        9, 9, 7, 9,  5, 13,  5, 3, 200));
 
 // CENARIOS DIFÍCEIS
-        this.cenariosDificeis.add(new Cenarios("Desarmar ogiva nuclear", 4, 6, 8, 5, 10, 15, 10, 2, 400));
-        this.cenariosDificeis.add(new Cenarios("Ajudar Velho Pucrson: O confronto final no .env", 6, 6, 7, 6, 8, 12, 8, 3, 450));
-        this.cenariosDificeis.add(new Cenarios("Derrotar o Bruto louco com composto V", 9, 6, 4, 8, 6, 14, 6, 3, 500));
-        this.cenariosDificeis.add(new Cenarios("Impedir guerra entre supers", 8, 8, 8, 8, 12, 20, 12, 4, 600));
-        this.cenariosDificeis.add(new Cenarios("Impedir vulcao em erupcao", 9, 5, 5, 9, 15, 18, 15, 2, 500));
+        this.cenariosDificeis.add(new Cenarios("Desarmar ogiva nuclear",                    7, 10, 13,  8, 10, 15, 10, 3, 400));
+        this.cenariosDificeis.add(new Cenarios("Ajudar Velho Pucrson: O confronto final no .env", 10, 10, 12, 10, 8, 12, 8, 3, 450));
+        this.cenariosDificeis.add(new Cenarios("Derrotar o Bruto louco com composto V",    14,  9,  6, 13,  6, 14,  6, 3, 500));
+        this.cenariosDificeis.add(new Cenarios("Impedir guerra entre supers",               13, 13, 13, 13, 12, 20, 12, 4, 600));
+        this.cenariosDificeis.add(new Cenarios("Impedir vulcao em erupcao",                 14,  8,  8, 14, 15, 18, 15, 3, 500));
     }
 
     // ESPECIAIS
